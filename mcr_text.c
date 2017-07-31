99f871544080f4edb7e319d415276078572598ea
Jeesyn <jeesyn.liu@gmail.com>
2017-07-31 11:17:18 +0800

    modify style

--- mcr_text.c
+++ mcr_text.c
@@ -3,41 +3,39 @@
 #include "include/mcr_define.h"
 
 
-
 char*
 readline(const char* buf, int * len)
 {
-	char * pos = (char*)buf;
-	while (*pos != '\0' && *pos != '\n') {
-			pos ++;
-	}
+    char * pos = (char*)buf;
+    while (*pos != '\0' && *pos != '\n') {
+        pos ++;
+    }
 
-	*len = pos - buf;
+    *len = pos - buf;
 
-	return (*pos == '\0') ? NULL : (pos + 1);
+    return (*pos == '\0') ? NULL : (pos + 1);
 }
 
 
 int
 str_split2(const char* buf, char* seperator, char* s1, char* s2)
 {
-
     char* s = strstr(buf, seperator);
     if (s == NULL) {
         return -1;
     }
 
-	/* more than one seperator existence is not allowed */
-	int l1 = s - buf;
-	if (strlen(buf) > l1 && NULL != strstr(s + strlen(seperator), seperator)) {
-		return -1;
-	}
+    /* more than one seperator existence is not allowed */
+    int l1 = s - buf;
+    if (strlen(buf) > l1 && NULL != strstr(s + strlen(seperator), seperator)) {
+        return -1;
+    }
 
 
-	if (l1 > 0) {
-		memcpy(s1, buf, l1);
-		s1[l1] = '\0';
-	}
+    if (l1 > 0) {
+        memcpy(s1, buf, l1);
+        s1[l1] = '\0';
+    }
 
     strcpy(s2, s +strlen(seperator));
 
@@ -50,18 +48,18 @@ to_words(const char* buf, char **words, int w_maxnum)
 {
     char* p, * w_start, * w_end;
     int i, l;
-	char *buf_end = strchr(buf, '\0');
+    char *buf_end = strchr(buf, '\0');
 
     for (p = (char*)buf, i =0; (p < buf_end )&&(i < w_maxnum); i++) {
         /* find word start */
         for (w_start = p; w_start < buf_end; w_start ++) {
             if ((*w_start >= 'a' && *w_start <= 'z') ||
-				(*w_start >= 'A' && *w_start <= 'Z') ||
-				(*w_start == '_'))
+                (*w_start >= 'A' && *w_start <= 'Z') ||
+                (*w_start == '_'))
                 break;
         }
 
-		/* no word left */
+        /* no word left */
         if (w_start == buf_end) {
             break;
         }
@@ -73,30 +71,30 @@ to_words(const char* buf, char **words, int w_maxnum)
                 (*w_end == '\n') ||
                 (*w_end == '\0') )
                 break;
-			
-			else if ((*w_end == '.') ||
-					 (*w_end == ',') ||
-					 (*w_end == '!') ||
-					 (*w_end == '?')) {
 
-				if ((*(w_end + 1) == ' ')  ||
-					(*(w_end + 1) == '\t') ||
-					(*(w_end + 1) == '\n') ||
-					(*(w_end + i) == '\0') ) {
-					break;
-				}
-			}
-		}
+            else if ((*w_end == '.') ||
+                     (*w_end == ',') ||
+                     (*w_end == '!') ||
+                     (*w_end == '?')) {
+
+                if ((*(w_end + 1) == ' ')  ||
+                    (*(w_end + 1) == '\t') ||
+                    (*(w_end + 1) == '\n') ||
+                    (*(w_end + i) == '\0') ) {
+                    break;
+                }
+            }
+        }
 
         l = w_end - w_start;
 
-		if (l > MAX_WORD_SIZE - 1) {
-			l = MAX_WORD_SIZE - 1;
-		}
+        if (l > MAX_WORD_SIZE - 1) {
+            l = MAX_WORD_SIZE - 1;
+        }
 
         strncpy(*(words + i), w_start , l);
-		/* force words endup with '\0' */
-		*(*(words + i) + l) = '\0';
+        /* force words endup with '\0' */
+        *(*(words + i) + l) = '\0';
 
         p = w_end;
     }
@@ -108,62 +106,61 @@ to_words(const char* buf, char **words, int w_maxnum)
 #ifdef TEST
 #include <stdlib.h>
 int main(int argc, char** argv) 
-{
-	/* test realine */
-	char rl_test[] = "aaaaaaaaaaaa\nbbbbbbbbbbbb\n\n  \n";
-	int len = 0;
-	char * s = rl_test;
-	char tmp[128];
-	char *nl;
-	while (1) {
-		nl = readline(s, &len);
-		memcpy(tmp, s, len);
-		tmp[len]= '\0';
-		printf("line: %s\n", tmp);
-		
-		if (nl == NULL) {
-			break;
-		}
-		s = nl;
-	}
+    {
+    /* test realine */
+    char rl_test[] = "aaaaaaaaaaaa\nbbbbbbbbbbbb\n\n  \n";
+    int len = 0;
+    char * s = rl_test;
+    char tmp[128];
+    char *nl;
+    while (1) {
+        nl = readline(s, &len);
+        memcpy(tmp, s, len);
+        tmp[len]= '\0';
+        printf("line: %s\n", tmp);
+
+        if (nl == NULL) {
+            break;
+        }
+        s = nl;
+    }
 
 
-	/* test seperator */
-	char spl_test[] = "lover: bing";
-	char spl_test1[] = ":bing";
-	char spl_test2[] = "lover:";
-	char s1[128], s2[128];
-	str_split2(spl_test, ":", s1, s2);
-	printf ("%s\n", s1);
-	printf ("%s\n", s2);
-	str_split2(spl_test1, ":", s1, s2);
-	printf ("%s\n", s1);
-	printf ("%s\n", s2);
-	str_split2(spl_test2, ":", s1, s2);
-	printf ("%s\n", s1);
-	printf ("%s\n", s2);
+    /* test seperator */
+    char spl_test[] = "lover: bing";
+    char spl_test1[] = ":bing";
+    char spl_test2[] = "lover:";
+    char s1[128], s2[128];
+    str_split2(spl_test, ":", s1, s2);
+    printf ("%s\n", s1);
+    printf ("%s\n", s2);
+    str_split2(spl_test1, ":", s1, s2);
+    printf ("%s\n", s1);
+    printf ("%s\n", s2);
+    str_split2(spl_test2, ":", s1, s2);
+    printf ("%s\n", s1);
+    printf ("%s\n", s2);
 
 
-	/* test to_words */
-	char **words = (char**)malloc(sizeof(char*)*20);
-	for (int i = 0; i < 20 ; i++) {
-		*(words + i) = (char*)malloc(sizeof(char)*128);
-	}
-	int ret = to_words("China is the greatest country in the world! do you agree? _QNMLGBD\n", words, 20);
-	printf("to_words ret:%d\n", ret);
-	for ( int i = 0; i < 20; i++ ) {
-		if (strlen(*(words+i)) == 0)
-			continue;
-		printf("i=%d, word=%s\n", i, *(words +i));
-	}
+    /* test to_words */
+    char **words = (char**)malloc(sizeof(char*)*20);
+    for (int i = 0; i < 20 ; i++) {
+        *(words + i) = (char*)malloc(sizeof(char)*128);
+    }
+    int ret = to_words("China is the greatest country in the world! do you agree? _QNMLGBD\n", words, 20);
+    printf("to_words ret:%d\n", ret);
+    for ( int i = 0; i < 20; i++ ) {
+        if (strlen(*(words+i)) == 0)
+            continue;
+        printf("i=%d, word=%s\n", i, *(words +i));
+    }
 
+    for (int i = 0; i < 20; i++) {
+        free(*(words + i));
+    }
+    free(words);
 
-	for (int i = 0; i < 20; i++) {
-		free(*(words + i));
-	}
-	free(words);
-
-	return 0;
+    return 0;
 }
 
 #endif
